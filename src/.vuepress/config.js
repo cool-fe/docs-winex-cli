33864ca07dccb2625be0c61cfd54b47e65e83a4d
GGwujun <18570148691@163.com>
2021-06-15 15:49:41 +0800

    doc: add migration

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,15 +1,15 @@
-const { description } = require('../../package')
+const { description } = require("../../package");
 
 module.exports = {
-  base:'/docs-winex-cli/',
+  base: "/docs-winex-cli/",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'WINEX CLI',
+  title: "WINEX CLI",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
-  description: 'a tool is the Standard Tooling for winfe',
+  description: "a tool is the Standard Tooling for winfe",
 
   /**
    * Extra tags to be injected to the page HTML `<head>`
@@ -17,9 +17,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
   ],
 
   /**
@@ -27,82 +30,95 @@ module.exports = {
    *
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
-  theme: '@vuepress/theme-vue',
+  theme: "@vuepress/theme-vue",
   themeConfig: {
-    repo: 'cool-fe/winex-cli',
+    repo: "cool-fe/winex-cli",
     editLinks: true,
-    editLinkText: '在 GitHub 上编辑此页',
-    lastUpdated: '上次更新',
-    docsRepo: 'cool-fe/docs-winex-cli',
-    docsDir: 'src',
+    editLinkText: "在 GitHub 上编辑此页",
+    lastUpdated: "上次更新",
+    docsRepo: "cool-fe/docs-winex-cli",
+    docsDir: "src",
     sidebarDepth: 2,
     nav: [
       {
-        text: '指南',
-        link: '/guide/',
+        text: "指南",
+        link: "/guide/",
       },
       {
-        text: 'lint配置指南',
-        link: '/config/',
+        text: "升级指南",
+        link: "/migration/",
       },
       {
-        text: '物料开发',
-        link: 'https://cool-fe.github.io/winex-material-doc/guides/',
+        text: "相关文档",
+        ariaLabel: "相关生态",
+        items: [
+          {
+            text: "lint配置指南",
+            link: "/config/",
+          },
+          {
+            text: "物料开发",
+            link: "https://cool-fe.github.io/winex-material-doc/guides/",
+          },
+        ],
       },
-      
     ],
     sidebar: {
-      '/guide/': [
-        '/guide/',
-        '/guide/install',
-        '/guide/init',
-        '/guide/add',
-        '/guide/lint'
+      "/guide/": [
+        "/guide/",
+        "/guide/install",
+        "/guide/init",
+        "/guide/add",
+        "/guide/lint",
       ],
-      '/config/': [
-        '/config/',
-        '/config/eslint',
-        '/config/prettier',
-        '/config/husky',
-        '/config/vscode',
+      "/config/": [
+        "/config/",
+        "/config/eslint",
+        "/config/prettier",
+        "/config/husky",
+        "/config/vscode",
       ],
-    }
+      "/migration/": [
+        "/migration/",
+        // '/migration/prettier',
+      ],
+    },
   },
 
-  dest: 'public',
+  dest: "public",
   configureWebpack: {
     resolve: {
       alias: {
-        '@assets': '/docs-winex-cli/'
-      }
-    }
+        "@assets": "/docs-winex-cli/",
+      },
+    },
   },
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
     [
-      '@vuepress/pwa',
+      "@vuepress/pwa",
       {
         serviceWorker: true,
         updatePopup: {
-          '/': {
-            message: '新内容可用',
-            buttonText: '刷新'
-          }
-        }
-      }
+          "/": {
+            message: "新内容可用",
+            buttonText: "刷新",
+          },
+        },
+      },
     ],
     [
-      'vuepress-plugin-container',
+      "vuepress-plugin-container",
       {
-        type: 'info',
-        before: info =>
+        type: "info",
+        before: (info) =>
           `<div class="custom-block info"><p class="custom-block-title">${info}</p>`,
-        after: '</div>'
-      }
+        after: "</div>",
+      },
     ],
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
-}
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
+  ],
+};
